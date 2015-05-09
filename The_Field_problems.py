fe2bd7a09ab96a2cb019196c7755e4f538f0c52a
gruzzlymug <kevin@gruzzlymug.com>
2015-05-08 23:06:35 -0400

    finish week 0 hw 4

--- The_Field_problems.py
+++ The_Field_problems.py
@@ -170,10 +170,10 @@ Be sure your procedure works for the empty list.
 ## 9: (Problem 9) Complex Addition Practice
 # Each answer should be a Python expression whose value is a complex number.
 
-complex_addition_a = ...
-complex_addition_b = ...
-complex_addition_c = ...
-complex_addition_d = ...
+complex_addition_a = (3+1j)+(2+2j)
+complex_addition_b = (-1+2j)+(1-1j)
+complex_addition_c = (2+0j)+(-3+0.001j)
+complex_addition_d = 4*(0+2j)+(0.001+1j)
 
 
 
@@ -192,12 +192,15 @@ def transform(a, b, L):
     >>> transform(3,2,[1,2,3])
     [5, 8, 11]
     '''
-    pass
+    current = []
+    for z in L:
+        current.append(a*z + b)
+    return current
 
 
 
 ## 11: (Problem 11) GF(2) Arithmetic
-GF2_sum_1 = ... # answer with 0 or 1
-GF2_sum_2 = ...
-GF2_sum_3 = ...
+GF2_sum_1 = 1   # answer with 0 or 1
+GF2_sum_2 = 0
+GF2_sum_3 = 0
 
